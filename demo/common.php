4231218b04150c2fb0047a699474afea1395a8f0
Nikita Makarov <Nikita.Makarov@effective-soft.com>
2014-03-12 14:34:34 +0300

    Demo Fixed

---
+++ demo/common.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Example Application
+ *
+ * @package Example-application
+ */
+$smarty = new Smarty;
+
+$smarty->setCacheDir(sys_get_temp_dir());
+$smarty->setCompileDir(sys_get_temp_dir());
+$smarty->addTemplateDir(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'templates');
+$smarty->setConfigDir(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'configs');
+$smarty->addPluginsDir(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'plugins');
+
+$smarty->debugging = true;
+//Note: That "debug.tpl" inside phar is not accessible))
+$smarty->debug_tpl = SMARTY_DEBUG_TPL;
+$smarty->caching = false;
+$smarty->cache_lifetime = 1;
+
+$smarty->assign("Name", "Fred Irving Johnathan Bradley Peppergill", true);
+$smarty->assign("FirstName", array("John", "Mary", "James", "Henry"));
+$smarty->assign("LastName", array("Doe", "Smith", "Johnson", "Case"));
+$smarty->assign("Class", array(array("A", "B", "C", "D"), array("E", "F", "G", "H"),
+    array("I", "J", "K", "L"), array("M", "N", "O", "P")));
+
+$smarty->assign("contacts", array(array("phone" => "1", "fax" => "2", "cell" => "3"),
+    array("phone" => "555-4444", "fax" => "555-3333", "cell" => "760-1234")));
+
+$smarty->assign("option_values", array("NY", "NE", "KS", "IA", "OK", "TX"));
+$smarty->assign("option_output", array("New York", "Nebraska", "Kansas", "Iowa", "Oklahoma", "Texas"));
+$smarty->assign("option_selected", "NE");
+
+$smarty->display('index.tpl', time(), time());
--- demo/test.gz.php
+++ demo/test.gz.php
@@ -5,26 +5,5 @@
  * @package Example-application
  */
 
-require_once 'phar://./../compiled/smarty.gz';
-
-$smarty = new Smarty;
-$smarty->debugging = true;
-//Note: That "debug.tpl" inside phar is not accessible))
-$smarty->debug_tpl = SMARTY_DEBUG_TPL;
-$smarty->caching = false;
-$smarty->cache_lifetime = 1;
-
-$smarty->assign("Name","Fred Irving Johnathan Bradley Peppergill",true);
-$smarty->assign("FirstName",array("John","Mary","James","Henry"));
-$smarty->assign("LastName",array("Doe","Smith","Johnson","Case"));
-$smarty->assign("Class",array(array("A","B","C","D"), array("E", "F", "G", "H"),
-	  array("I", "J", "K", "L"), array("M", "N", "O", "P")));
-
-$smarty->assign("contacts", array(array("phone" => "1", "fax" => "2", "cell" => "3"),
-	  array("phone" => "555-4444", "fax" => "555-3333", "cell" => "760-1234")));
-
-$smarty->assign("option_values", array("NY","NE","KS","IA","OK","TX"));
-$smarty->assign("option_output", array("New York","Nebraska","Kansas","Iowa","Oklahoma","Texas"));
-$smarty->assign("option_selected", "NE");
-
-$smarty->display('index.tpl');
+require_once 'phar://' . dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'compiled' . DIRECTORY_SEPARATOR . 'smarty.gz';
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR .'commn.php';
